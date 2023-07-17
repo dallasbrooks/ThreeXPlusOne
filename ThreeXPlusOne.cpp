2ce3589551612e9a9295bad23d5606330850bf0e
Dallas Brooks <47010213+dallasbrooks@users.noreply.github.com>
2023-07-16 21:22:15 -0700

    Add files via upload

---
+++ ThreeXPlusOne.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#define N 100
+
+void ThreeXPlusOne(long n, long& c)
+{
+    if (n <= 1)
+    {
+        return;
+    }
+    n = n%2 ? 3*n+1 : n/2;
+    return ThreeXPlusOne(n, ++c);
+}
+
+int main() 
+{
+    for (long i = 0; i < N; ++i)
+    {
+        long count = 0;
+        ThreeXPlusOne(i, count);
+        std::cout << i << ": " << count << std::endl;
+    }
+    return 0;
+}
